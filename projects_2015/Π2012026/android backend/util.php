5f3fdbf285e3e3d6b5b8679180e37fcd5d61b3b3
mike <p12kora@gmail.com>
2015-04-30 20:00:44 +0300

    remove code

--- projects_2015/Π2012026/android backend/util.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  include_once('connect2db.php');
-  include_once('create_db.php');
-
-?>
--- projects_2015/Π2012026/fb_flutrack/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-session_destroy();
-
-
-header( 'Location: http://localhost/fb_flutrack/index.php' );
-die();
-?>
